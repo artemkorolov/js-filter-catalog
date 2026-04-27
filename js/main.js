4ec3a67e361aa77252947a60ed6529ad05bbc8ed
Artem Korolov <adkorolov@gmail.com>
2026-04-27 11:41:17 +0300

    chore: initial project structure with html, css and js

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const products = [
+	{
+		id: 1,
+		title: "Nike Air Max",
+		brand: "Nike",
+		price: 150,
+		category: "Running",
+		inStock: true,
+		image: "https://placehold.co/200"
+	},
+	{
+		id: 2,
+		title: "Adidas Forum",
+		brand: "Adidas",
+		price: 90,
+		category: "Casual",
+		inStock: true,
+		image: "https://placehold.co/200"
+	},
+	{
+		id: 3,
+		title: "Puma Cali Special",
+		brand: "Puma",
+		price: 85,
+		category: "Casual",
+		inStock: false,
+		image: "https://placehold.co/200"
+	},
+	{
+		id: 4,
+		title: "New Balance 530",
+		brand: "New Balance",
+		price: 125,
+		category: "Running",
+		inStock: true,
+		image: "https://placehold.co/200"
+	},
+	{
+		id: 5,
+		title: "Jordan Retro 1",
+		brand: "Jordan",
+		price: 180,
+		category: "Basketball",
+		inStock: true,
+		image: "https://placehold.co/200"
+	},
+	{
+		id: 6,
+		title: "Reebok Classic",
+		brand: "Reebok",
+		price: 75,
+		category: "Casual",
+		inStock: true,
+		image: "https://placehold.co/200"
+	},
+];
+
+console.log("Data loaded:", products);
